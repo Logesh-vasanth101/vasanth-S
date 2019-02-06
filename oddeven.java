9aab17674ff7c65328ee734ae29f6e19681b4be5
Logesh-vasanth101 <47377120+Logesh-vasanth101@users.noreply.github.com>
2019-02-06 13:49:17 +0530

    Create oddeven.java

---
+++ oddeven.java
@@ -0,0 +1,19 @@
+ class PositiveNegative
+ {
+
+    public static void main(String[] args)
+    {
+
+       int number = 3;
+        System.out.println("Input:");
+        
+        if (number % 2== 0)
+            System.out.print(number + " \nOutput:\nEven");
+
+      
+        else  
+            System.out.print(number + "\nOutput:\nOdd");
+
+      
+    }
+}
