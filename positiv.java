e011ea803edf87fd394610dd0c01fb4a6bb35fc5
Logesh-vasanth101 <47377120+Logesh-vasanth101@users.noreply.github.com>
2019-02-06 12:19:24 +0530

    Create positiv.java

---
+++ positiv.java
@@ -0,0 +1,19 @@
+ public class PositiveNegative {
+
+    public static void main(String[] args) {
+
+       int number = 3;
+        System.out.println("Input:");
+        
+        if (number < 0)
+            System.out.print(number + " \nOutput:\nNegative");
+
+      
+        else if ( number > 0)
+            System.out.print(number + "\nOutput:\nPositive");
+
+      
+        else
+            System.out.print(number + "\nOutput:\nZero");
+    }
+}
